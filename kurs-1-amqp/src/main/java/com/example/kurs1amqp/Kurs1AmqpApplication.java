2505c05f8e04493181cce49a2a10374483b46686
lshadown <gruzewski94@gmail.com>
2020-05-01 19:23:38 +0200

    init repo

---
+++ kurs-1-amqp/src/main/java/com/example/kurs1amqp/Kurs1AmqpApplication.java
@@ -0,0 +1,13 @@
+package com.example.kurs1amqp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kurs1AmqpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kurs1AmqpApplication.class, args);
+    }
+
+}
---
+++ kurs-1-app/src/main/java/com/example/kurs1app/Kurs1AppApplication.java
@@ -0,0 +1,13 @@
+package com.example.kurs1app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kurs1AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kurs1AppApplication.class, args);
+	}
+
+}
